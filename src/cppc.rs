01eb5e9618144330bf4cdfd4e5b944e906030ca9
lowa9 <suxiaokai34@gmail.com>
2023-11-10 02:18:48 +0800

    cppc: 添加SBI CPPC扩展的支持
    Signed-off-by: lowa9 <suxiaokai34@gmail.com>

---
+++ src/cppc.rs
@@ -0,0 +1,33 @@
+//! Chapter 14. CPPC Extension (EID #0x43505043 "CPPC")
+
+#[cfg(target_pointer_width = "64")]
+use crate::binary::sbi_call_2;
+#[cfg(target_pointer_width = "32")]
+use crate::binary::sbi_call_3;
+use crate::binary::{sbi_call_1, SbiRet};
+use sbi_spec::cppc::{EID_CPPC, PROBE};
+
+#[inline]
+pub fn cppc_probe(cppc_reg_id: usize) -> SbiRet {
+    sbi_call_1(EID_CPPC, PROBE, cppc_reg_id)
+}
+
+#[inline]
+pub fn cppc_read(cppc_reg_id: usize) -> SbiRet {
+    sbi_call_1(EID_CPPC, PROBE, cppc_reg_id)
+}
+
+#[inline]
+pub fn cppc_read_hi(cppc_reg_id: usize) -> SbiRet {
+    sbi_call_1(EID_CPPC, PROBE, cppc_reg_id)
+}
+
+#[inline]
+pub fn cppc_write(cppc_reg_id: usize, value: u64) -> SbiRet {
+    match () {
+        #[cfg(target_pointer_width = "32")]
+        () => sbi_call_3(EID_CPPC, PROBE, cppc_reg_id, value as _, (value >> 32) as _),
+        #[cfg(target_pointer_width = "64")]
+        () => sbi_call_2(EID_CPPC, PROBE, cppc_reg_id, value as _),
+    }
+}
